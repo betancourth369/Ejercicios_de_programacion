8b0c4f7e30ed5977fb466961c3f5f898260d9541
Pablo Betancourth <pablobetancourth00@gmail.com>
2026-05-26 19:17:49 +0200

    implementacion metodo elijeJuego, pendiente validaciongit add juego/

--- Back-end/juego/src/Aplicacion/java/Main.java
+++ Back-end/juego/src/Aplicacion/java/Main.java
@@ -12,37 +12,48 @@ public class Main {
     private static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
+        //pendiente ...
+        eligeJuego();
 
+    }
 
-        public static Jugable eligeJuego(){
+    public static Jugable eligeJuego () {
 
-            JuegoAdivinaNumero jan = new JuegoAdivinaNumero(3,5);
-            JuegoAdivinaPar jap = new JuegoAdivinaPar(4,6);
-            JuegoAdivinaImpar jai = new JuegoAdivinaImpar(5,7);
+        JuegoAdivinaNumero jan = new JuegoAdivinaNumero(5, 5);
+        JuegoAdivinaPar jap = new JuegoAdivinaPar(5, 6);
+        JuegoAdivinaImpar jai = new JuegoAdivinaImpar(5, 7);
 
-            Jugable[] juegos = new Jugable[3];
-            juegos[0] = jan;
-            juegos[1] = jap;
-            juegos[2] = jai;
+        Jugable[] juegos = new Jugable[3];
+        juegos[0] = jan;
+        juegos[1] = jap;
+        juegos[2] = jai;
 
-            boolean juegoCorrecto = false;
-            Jugable respuesta = null;
+        boolean juegoCorrecto = false;
+        Jugable respuesta = null;
 
-            do {
-                System.out.println("ELIJE UN JUEGO!!");
-                for (int i = 0; i < juegos.length; i++){
+        do {
+            System.out.println("ELIJE UN JUEGO!!");
+            for (int i = 0; i < juegos.length; i++) {
 
-                }
+                Jugable j = juegos[i];
+                System.out.println(i + "_");
+                j.muestraNombre();
+
             }
 
+            int juegoelegido = teclado.nextInt();
 
+            if (juegoelegido >= 0 && juegoelegido < juegos.length) {
+                juegoCorrecto = true;
+                respuesta = juegos[juegoelegido];
+            } else {
+                juegoCorrecto = false;
+                System.out.println("Elije un juego correcto");
+            }
 
-        }
+        } while (!juegoCorrecto);
 
-
-
-
-
+        return respuesta;
 
 
     }
